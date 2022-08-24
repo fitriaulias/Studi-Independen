c5d14154e907df1be40588d91b84498fc277384e
fitriaulias <fitriauliasanni@gmail.com>
2022-08-24 10:38:37 +0700

    first commit

---
+++ Sesi 1/hello-nodemon/index.js
@@ -0,0 +1,15 @@
+//npm init -y
+//install nodemon -g > global // -d > local
+//untuk running nodemin ? di powershell ga bisa
+//running menggunakan cmd / command prompt
+//1. nodemon > global
+//2. npm run start > kita tambahkan script running 
+//ex script : "start : "nodemon.js > package.json
+
+let message = 'Welcome\'s'
+console.log(message);
+
+let originalNum = 23
+let numberToBeAdded = 7
+let newNum = originalNum
+console.log(originalNum)
