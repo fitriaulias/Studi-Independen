ed26a7a86cbcf334b4250382289a990d51509643
fitriaulias <fitriauliasanni@gmail.com>
2022-09-12 21:54:49 +0700

    assignment2

--- assignment-02/currency-rates/src/components/currency.js
+++ assignment-02/currency-rates/src/components/currency.js
@@ -7,7 +7,7 @@ import Axios from 'axios';
 const Currency = () => {
     const [ currency, setCurrency ] = useState({});
 
-    //Memanggil API
+    //Memanggil API menggunakan UseEffect
     useEffect(() => {
         Axios.get(
             'https://api.currencyfreaks.com/latest?apikey=353a6d13ec224c7789b3ec97c17fa95b&symbols=CAD,IDR,JPY,CHF,EUR,GBP')
@@ -19,19 +19,23 @@ const Currency = () => {
     return (
         <>
         <table>
+            <thead>
             <tr>
                 <th>Currency</th>
                 <th>We Buy</th>
                 <th>Exchange Rate</th>
                 <th>We Sell</th>
             </tr>
+            </thead>
+            <tbody>
             {Object.entries(currency.rates ?? {}).map(([key, value]) => (
-                <tr>
+                <tr key={key}>
                     <td>{key}</td>
                     <td>{Math.round((parseFloat(value) + (parseFloat(value) * 5/100))*10000)/10000}</td>
                     <td>{Math.round((parseFloat(value))*1000000)/1000000}</td>
-                    <td>{Math.round((parseFloat(value) - (parseFloat(value) * 5/100))*1000000)/1000000}</td>
+                    <td>{Math.round((parseFloat(value) - (parseFloat(value) * 5/100))*10000)/10000}</td>
                 </tr>))}
+            </tbody>
         </table>
         </>
     )
